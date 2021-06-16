311d4dde77f3bb8585aa2d1a3188760972d0ab83
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-06-16 11:19:41 +0300

    update structure

--- src/main/java/com/epam/laboratory/XMLtoJSONConverter.java
+++ src/main/java/com/epam/laboratory/XMLtoJSONConverter.java
@@ -11,8 +11,9 @@ public class XMLtoJSONConverter {
     private static final Logger LOGGER = Logger.getLogger(XMLtoJSONConverter.class);
 
     private static final int INDENT_FACTOR = 3;
+    private static JSONObject jsonObject;
 
-    public void convert(String PATH_TO_XML_FILE, String PATH_TO_JSON_FILE) {
+    public static JSONObject getJsonObject(String PATH_TO_XML_FILE) {
         try {
             File xmlFile = new File(PATH_TO_XML_FILE);
             InputStream inputStream = new FileInputStream(xmlFile);
@@ -23,13 +24,21 @@ public class XMLtoJSONConverter {
             }
 
             String xml = builder.toString();
-            JSONObject jsonObj = XML.toJSONObject(xml);
+            jsonObject = XML.toJSONObject(xml);
+        } catch (IOException ex) {
+        LOGGER.error("Error reading from file '" + PATH_TO_XML_FILE + "'");
+    }
+        return jsonObject;
+    }
+
+    public void convert(String PATH_TO_XML_FILE, String PATH_TO_JSON_FILE) {
+        try {
             FileWriter fileWriter =
                     new FileWriter(PATH_TO_JSON_FILE);
 
             BufferedWriter bufferedWriter =
                     new BufferedWriter(fileWriter);
-            bufferedWriter.write(jsonObj.toString(INDENT_FACTOR));
+            bufferedWriter.write(getJsonObject(PATH_TO_XML_FILE).toString(INDENT_FACTOR));
             bufferedWriter.close();
         } catch (IOException ex) {
             LOGGER.error("Error writing to file '" + PATH_TO_JSON_FILE + "'");
--- src/main/java/com/epam/laboratory/workObjects/DiamondFund.java
+++ src/main/java/com/epam/laboratory/workObjects/DiamondFund.java
@@ -1,17 +1,10 @@
 package com.epam.laboratory.workObjects;
 
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-@XmlRootElement(name = "DiamondFund")
-@XmlAccessorType(XmlAccessType.FIELD)
+
 public class DiamondFund {
 
-    @XmlElement(name = "gem")
     private List<Gem> gems;
 
     public List<Gem> getGems() {
