311d7bff1941352c1f5740324edb6dd0d4e1fe3a
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-06-30 11:04:48 +0300

    reformat resources

--- src/main/java/com/epam/laboratory/Config.java
+++ src/main/java/com/epam/laboratory/Config.java
@@ -10,9 +10,9 @@ import java.util.Properties;
 public class Config {
     private static final String PROPERTIES_FILE = "src/main/resources/paths.properties";
 
-    public static String PATH_TO_XML_FILE;
-    public static String PATH_TO_JSON_FILE;
-    public static String PATH_TO_XSD_FILE;
+    public static String pathToXmlFile;
+    public static String pathToJsonFile;
+    public static String pathToXsdFile;
     private static final Logger LOGGER = Logger.getLogger(Config.class);
 
     static {
@@ -20,9 +20,9 @@ public class Config {
             Properties properties = new Properties();
             properties.load(propertiesFile);
 
-            PATH_TO_XML_FILE = properties.getProperty("PATH_TO_XML_FILE");
-            PATH_TO_JSON_FILE = properties.getProperty("PATH_TO_JSON_FILE");
-            PATH_TO_XSD_FILE = properties.getProperty("PATH_TO_XSD_FILE");
+            pathToXmlFile = properties.getProperty("PATH_TO_XML_FILE");
+            pathToJsonFile = properties.getProperty("PATH_TO_JSON_FILE");
+            pathToXsdFile = properties.getProperty("PATH_TO_XSD_FILE");
 
         } catch (FileNotFoundException ex) {
             LOGGER.error("Properties config file not found. " + ex.getMessage());
